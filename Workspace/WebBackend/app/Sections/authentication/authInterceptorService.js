763bd6358a3e6d580e040821bed5676bfebd4944
daiduongbian44 <nguyenquangmanh9099@gmail.com>
2016-03-06 00:38:50 +0700

    Commit đủ file cho tính năng đăng nhập

---
+++ Workspace/WebBackend/app/Sections/authentication/authInterceptorService.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+app.factory('authInterceptorService', ['$q', '$injector', '$location', 'localStorageService', function ($q, $injector, $location, localStorageService) {
+
+    var authInterceptorServiceFactory = {
+        request: _request,
+        responseError: _responseError,
+    };
+    return authInterceptorServiceFactory;
+
+    // each the request attach with access_token
+    var _request = function (config) {
+        config.headers = config.headers || {};
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            config.headers.Authorization = 'Bearer ' + authData.token;
+        }
+
+        return config;
+    }
+
+    // response error
+    var _responseError = function (rejection) {
+        if (rejection.status === 401) {
+            var authService = $injector.get('authService');
+
+            authService.LogOut();
+            $location.path('/login');
+        }
+        return $q.reject(rejection);
+    }
+}]);
---
+++ Workspace/WebBackend/app/Sections/authentication/signup/signupService.js
@@ -0,0 +1,43 @@
+﻿
+(function () {
+    'use strict';
+
+    angular.module('LibManageApp').factory('signupService', signupService);
+
+    signupService.$inject = ['$http', '$q', '$state', 'ngAuthSettings'];
+
+    function signupService($http, $q, ngAuthSettings) {
+
+        var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+        var Factory = {
+            SaveUser: _saveUser,
+        };
+        return Factory;
+
+        // save user-info
+        function _saveUser(user) {
+            var deferred = $q.defer();
+            var url = "/api/authen/saveuser";
+
+            $http.post(url,
+                JSON.stringify(user),
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            )
+            .success(function (response) {
+                deferred.resolve(response);
+                return response;
+            })
+            .error(function (errMessage, statusCode) {
+                var result = { isSuccess: false, status: statusCode, message: errMessage };
+                deferred.reject(result);
+                return result;
+            });
+
+            return deferred.promise;
+        }
+    }
+
+})();
