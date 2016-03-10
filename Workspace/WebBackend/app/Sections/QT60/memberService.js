34ffc3490feb5b6fc8e84cca3eadf0d329098ed7
SnowStom <tuandaotien@gmail.com>
2016-03-10 23:56:02 +0700

    update quan ly nguoi dung, tao moi nguoi dung da thanh cong, chuc nang edit dang co loi. P/s : fix loi tai anh kich thuoc lon mot vai van de: tai sao phai up anh moi tai 1 khung anh moi, ma ko cho up anh tai khung anh dai dien dang su dung?
    Bo sung ham EmailBll va EmailDAL. mau tao 1 email trong ham khi tao 1 user moi tai man hinh dang nhap

---
+++ Workspace/WebBackend/app/Sections/QT60/memberService.js
@@ -0,0 +1,90 @@
+﻿(function () {
+    'use strict'
+
+    angular.module('LibManageApp').factory('memberService', memberService);
+
+    memberService.$inject = ['$q', '$http', 'ngAuthSettings'];
+
+    function memberService($q, $http, ngAuthSettings) {
+
+        var Factory = {
+            GetAllUsers: _getAllUsers,
+            GetAllRoles: _getAllRoles,
+            SaveUser: _saveUser
+        };
+        return Factory;
+
+        // return all user from db
+        function _getAllUsers() {
+            var deferred = $q.defer();
+            var url = "/api/user/getusers";
+
+            $http.post(url,
+                null,
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            )
+            .success(function (response) {
+                deferred.resolve(response);
+                return response;
+            })
+            .error(function (errMessage, statusCode) {
+                var result = { isSuccess: false, status: statusCode, message: errMessage };
+                deferred.reject(result);
+                return result;
+            });
+
+            return deferred.promise;
+        }
+
+        // return all role from db
+        function _getAllRoles() {
+            var deferred = $q.defer();
+            var url = "/api/user/getroles";
+
+            $http.post(url,
+                null,
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            )
+            .success(function (response) {
+                deferred.resolve(response);
+                return response;
+            })
+            .error(function (errMessage, statusCode) {
+                var result = { isSuccess: false, status: statusCode, message: errMessage };
+                deferred.reject(result);
+                return result;
+            });
+
+            return deferred.promise;
+        }
+
+        // save an user to db
+        function _saveUser(user) {
+            var deferred = $q.defer();
+            var url = "/api/authen/saveuser";
+
+            $http.post(url,
+                JSON.stringify(user),
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            )
+            .success(function (response) {
+                deferred.resolve(response);
+                return response;
+            })
+            .error(function (errMessage, statusCode) {
+                var result = { isSuccess: false, status: statusCode, message: errMessage };
+                deferred.reject(result);
+                return result;
+            });
+
+            return deferred.promise;
+        }
+
+    }
+})();
