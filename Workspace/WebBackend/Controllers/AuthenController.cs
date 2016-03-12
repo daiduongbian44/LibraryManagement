1628cdb44ab5ddbe321c4f221bcb21a2fef7d3f5
SnowStom <tuandaotien@gmail.com>
2016-03-12 23:51:34 +0700

    update save email when registration a accoount

--- Workspace/WebBackend/Controllers/AuthenController.cs
+++ Workspace/WebBackend/Controllers/AuthenController.cs
@@ -60,18 +60,33 @@ namespace WebBackend.Controllers
                 EmailBLL emailBLL = new EmailBLL();
                 int value = bll.SaveUser(user);
 
-                EmailModel email = new EmailModel();
-                email.EmailTo = ConfigurationManager.AppSettings["EmailAdmin"];
-                email.Title = "Kích hoạt tài khoản mới!";
-                email.Body = "Tài khoản (" + user.UserName + ") vừa được tạo vào lúc " + DateTime.Now.ToString() + ", yêu cầu cấp quyền quản lý. Hãy truy cập vào trang web để xử lý <br /> Chân trọng thông báo!";
-                email.IsSent = false;
-                email.EmailID = 0;
-                emailBLL.SaveEmail(email);
-
                 if (value <= 0)
                 {
                     result.Status = Constant.API_RESULT_ERROR;
-                    result.Messages = "Người dùng đã tồn tại.";
+                    result.Messages = "Thông tin về người dùng: username/email/phone là duy nhất. Hãy kiểm tra lại.";
+                }
+                else
+                {
+                    EmailModel email = new EmailModel();
+                    string RoleName = "";
+                    switch (user.RoleID)
+                    {
+                        case 1:
+                            RoleName = "quản trị hệ thống";
+                            break;
+                        case 2:
+                            RoleName = "cán bộ quản lý";
+                            break;
+                        case 3:
+                            RoleName = "người đọc";
+                            break;
+                    }
+                    email.EmailTo = ConfigurationManager.AppSettings["EmailAdmin"];
+                    email.Title = "Kích hoạt tài khoản mới!";
+                    email.Body = "Tài khoản (" + user.UserName + ") vừa được tạo vào lúc " + DateTime.Now.ToString() + ", yêu cầu cấp quyền " + RoleName + ". Hãy truy cập vào trang web để xử lý <br /> Chân trọng thông báo!";
+                    email.IsSent = false;
+                    email.EmailID = 0;
+                    emailBLL.SaveEmail(email);
                 }
             }
             catch (Exception ex)
