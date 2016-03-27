07bbd58d183eddbe3208e3197692aaf4fa7d48e8
SnowStorm <tuandaotien@gmail.com>
2016-03-27 22:12:45 +0700

    add @UserID param for cat_Save_Book proc

--- Workspace/DALs/BookDal.cs
+++ Workspace/DALs/BookDal.cs
@@ -29,13 +29,14 @@ namespace DALs
                 var param = new DynamicParameters();
                 param.Add("@BookID", book.BookID, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
                 param.Add("@XML", xml);
+                param.Add("@UserID", 1);
 
                 param.Add("@Return", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
                 var con = DatabaseContext.getInstance().Connection;
                 con.Execute(procName, param, commandType: CommandType.StoredProcedure);
                 return param.Get<int>("@BookID");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 throw;
             }
