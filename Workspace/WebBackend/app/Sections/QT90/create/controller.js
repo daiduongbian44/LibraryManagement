4e378d72e6c01bc847215cf94c14e586bc2ee431
daiduongbian44 <nguyenquangmanh9099@gmail.com>
2016-04-13 22:16:58 +0700

    Move Items

---
+++ Workspace/WebBackend/app/Sections/QT90/create/controller.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict'
+    angular.module('LibManageApp').controller('orderBorrowCreateController', orderBorrowCreateController);
+
+    orderBorrowCreateController.$inject = ['$scope', '$location', 'ngAuthSettings', '$ocLazyLoad', '$modal', 'DTOptionsBuilder'];
+
+    function orderBorrowCreateController($scope, $location, ngAuthSettings, $ocLazyLoad, $modal, DTOptionsBuilder) {
+                
+    }
+})();
